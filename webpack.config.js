c63ea13c90642edb4e289ef6c4c2afce170b070a
Grace Han <hanpanpan200@gmail.com>
2015-11-17 21:53:44 +0800

    feat: initialize repository

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+}
